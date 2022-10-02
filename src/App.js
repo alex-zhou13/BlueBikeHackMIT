e8a108d58eabd18ccc60a3b99e96d601dbc75357
alex-zhou13 <75346304+alex-zhou13@users.noreply.github.com>
2022-10-02 01:15:51 -0400

    set up front end, need to fetch data and style more

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import './App.css';
 import React, { useState } from 'react';
 
+const nearby_station_ids = new Array(179,184,178,380,67,80,566,479,107,318,471,472,189,72);
+const nearby_station_names = new Array("MIT Vassar St","Sidney Research Campus/Erie Street at Waverly","MIT Pacific St at Purrington St",
+"Mass Ave at Albany St","MIT at Mass Ave / Amherst St","MIT Stata Center at Vassar St / Main St","Main Street/Albany Street/Technology Square",
+"Galileo Galilei Way at Main Street","Ames St at Main St","Ames St at Broadway","MIT Carleton St at Amherst St","MIT Hayward St at Amherst St",
+"Kendall T","One Broadway / Kendall Sq at Main St / 3rd St");
+
 function App() {
   const [location, setLocation] = useState('');
   const [date, setDate] = useState("");
@@ -10,15 +16,15 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    setResponse("80% Probability of 1 Bike");
+    setResponse("TBD");
   }
 
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
-        <h1>When would you like to use a Blue Bike?</h1>
+        <h1>When and where would you like to use a Blue Bike?</h1>
 
-        <label for="location">Location:</label>
+        <label for="location">Select location:</label>
         <input
           type='text'
           onChange={(event) => {setLocation(event.target.value) }}
@@ -28,13 +34,23 @@ function App() {
           required
         />
         <datalist id="places">
-          <option>GSU</option>
-          <option>Boston University West</option>
-          <option>Boston University East</option>
-          <option>Warren Towers</option>
+          <option>{nearby_station_names[0]}</option>
+          <option>{nearby_station_names[1]}</option>
+          <option>{nearby_station_names[2]}</option>
+          <option>{nearby_station_names[3]}</option>
+          <option>{nearby_station_names[4]}</option>
+          <option>{nearby_station_names[5]}</option>
+          <option>{nearby_station_names[6]}</option>
+          <option>{nearby_station_names[7]}</option>
+          <option>{nearby_station_names[8]}</option>
+          <option>{nearby_station_names[9]}</option>
+          <option>{nearby_station_names[10]}</option>
+          <option>{nearby_station_names[11]}</option>
+          <option>{nearby_station_names[12]}</option>
+          <option>{nearby_station_names[13]}</option>
         </datalist>
 
-        <label for="date">Start date:</label>
+        <label for="date">Enter date:</label>
         <input type="date" id="date" name="trip-start"
           value="2022-10-01"
           min="2022-10-01" max="2023-12-31"
@@ -43,7 +59,7 @@ function App() {
         />
         <br/>
 
-        <label for="hour">Hour:</label>
+        <label for="hour">Enter time:</label>
         <input
           id="hour"
           type="time"
@@ -52,11 +68,19 @@ function App() {
           required
         />
         <br/>
+        <br/>
 
         <button type="submit">Submit</button>
       </form>
 
-      <h3>{response}</h3>
+      <h3>Predicted number of bikes: {response}</h3> <br/> 
+      <h3>Probability of finding a bike: {response}</h3> <br/> 
+      <h3>Predicted number of spots: {response}</h3> <br/> 
+      <h3>Probability of finding a spot: {response}</h3> <br/> 
+
+      <img class = "logo"
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Bluebikes_Logo.svg/2560px-Bluebikes_Logo.svg.png"/>
+
     </div>
   );
 }
