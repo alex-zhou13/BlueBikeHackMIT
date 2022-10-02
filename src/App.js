3953f9595f29504fa5153196bfd91612f3397ab0
alex-zhou13 <75346304+alex-zhou13@users.noreply.github.com>
2022-10-02 09:34:17 -0400

    more frontend for merging

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
 import React, { useState } from 'react';
+import bikePic from './Bike-icon.png';
 
 const nearby_station_ids = new Array(179,184,178,380,67,80,566,479,107,318,471,472,189,72);
 const nearby_station_names = new Array("MIT Vassar St","Sidney Research Campus/Erie Street at Waverly","MIT Pacific St at Purrington St",
 "Mass Ave at Albany St","MIT at Mass Ave / Amherst St","MIT Stata Center at Vassar St / Main St","Main Street/Albany Street/Technology Square",
 "Galileo Galilei Way at Main Street","Ames St at Main St","Ames St at Broadway","MIT Carleton St at Amherst St","MIT Hayward St at Amherst St",
 "Kendall T","One Broadway / Kendall Sq at Main St / 3rd St");
+
+const nearby_stations = [
+  {id: 179, name: "MIT Vassar St"},
+  {id: 184, name: "Sidney Research Campus/Erie Street at Waverly"},
+  {id: 178, name: "MIT Pacific St at Purrington St"},
+  {id: 380, name: "Mass Ave at Albany St"},
+  {id: 67, name: "MIT at Mass Ave / Amherst St"},
+  {id: 80, name: "MIT Stata Center at Vassar St / Main St"},
+  {id: 566, name: "Main Street/Albany Street/Technology Square"},
+  {id: 479, name: "Galileo Galilei Way at Main Street"},
+  {id: 107, name: "Ames St at Main St"},
+  {id: 318, name: "Ames St at Broadway"},
+  {id: 471, name: "MIT Carleton St at Amherst St"},
+  {id: 472, name: "MIT Hayward St at Amherst St"},
+  {id: 189, name: "Kendall T"},
+  {id: 72, name: "One Broadway / Kendall Sq at Main St / 3rd St"},
+];
 
 function App() {
   const [location, setLocation] = useState('');
@@ -22,7 +40,8 @@ function App() {
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
-        <h1>When and where would you like to use a Blue Bike?</h1>
+        <br />
+        <h1>When and where would you like to use a BlueBike?</h1> <br/>
 
         <label for="location">Select location:</label>
         <input
@@ -34,21 +53,11 @@ function App() {
           required
         />
         <datalist id="places">
-          <option>{nearby_station_names[0]}</option>
-          <option>{nearby_station_names[1]}</option>
-          <option>{nearby_station_names[2]}</option>
-          <option>{nearby_station_names[3]}</option>
-          <option>{nearby_station_names[4]}</option>
-          <option>{nearby_station_names[5]}</option>
-          <option>{nearby_station_names[6]}</option>
-          <option>{nearby_station_names[7]}</option>
-          <option>{nearby_station_names[8]}</option>
-          <option>{nearby_station_names[9]}</option>
-          <option>{nearby_station_names[10]}</option>
-          <option>{nearby_station_names[11]}</option>
-          <option>{nearby_station_names[12]}</option>
-          <option>{nearby_station_names[13]}</option>
+          {nearby_stations.map((nearby_station) => <option key={nearby_station.id} value={nearby_station.name}>{nearby_station.name}</option>)}
         </datalist>
+
+        
+
 
         <label for="date">Enter date:</label>
         <input type="date" id="date" name="trip-start"
@@ -73,13 +82,23 @@ function App() {
         <button type="submit">Submit</button>
       </form>
 
-      <h3>Predicted number of bikes: {response}</h3> <br/> 
-      <h3>Probability of finding a bike: {response}</h3> <br/> 
-      <h3>Predicted number of spots: {response}</h3> <br/> 
-      <h3>Probability of finding a spot: {response}</h3> <br/> 
+      <div class = "grid-container">
+        <div class="grid-item">
+          <img class = "bike" src = {bikePic} alt = "Bike!"/>
+        </div>
+        <div class="grid-item">
+          <h3>Predicted number of bikes: {response}</h3> <br/> 
+          <h3>Probability of finding a bike: {response}</h3> <br/> 
+          <h3>Predicted number of spots: {response}</h3> <br/> 
+          <h3>Probability of finding a spot: {response}</h3> <br/> 
+        </div>
+        <div class="grid-item">
+          <img class = "bike" src = {bikePic} alt = "Bike!"/>
+        </div>
+      </div>
 
       <img class = "logo"
-      src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Bluebikes_Logo.svg/2560px-Bluebikes_Logo.svg.png"/>
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Bluebikes_Logo.svg/2560px-Bluebikes_Logo.svg.png" alt = "BlueBikes!"/>
 
     </div>
   );
